9e7ea8c1f91e6a1d410f50b617dba88f2793f520
Jeremy <jeremy__francis@hotmail.com>
2022-07-19 09:04:47 -0700

    commit tree 1219ac6538101881abae7aa26bad532d5fe37c5a

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+greet("Naureen")
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default("Sunny")
+
+def add(num1, num2)
+    num1 + num2
+end 
+
+puts add(1,2)
+
+def halve(number)
+    if number.class != Integer 
+        return nil
+    end
+
+    number/2
+end
+
+puts halve(4)
